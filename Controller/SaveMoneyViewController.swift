0ac15d8a285be6b3df2e72061dbb8831b6218241
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-07-06 15:37:31 +0300

    Add files via upload

---
+++ Controller/SaveMoneyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SaveMoneyViewController.swift
+//  bankProjectCode
+//
+//  Created by Valentina Krysak on 6.07.22.
+//
+
+import UIKit
+
+class SaveMoneyViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemYellow
+
+    }
+    
+
+}
